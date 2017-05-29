d454312df3f9cc32e19abaf57027f8da8c0b2e69
Sunmi Kim <Sun@Sunmiui-MacBook-Air.local>
2017-05-30 06:53:06 +0900

    [refactor] react components 분리

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Game from './Game';
+import '../index.css';
+ 
+const App = () => {
+  return (
+    <div className="app">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css';
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+export default Square
