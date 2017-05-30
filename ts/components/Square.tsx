36804565f62c44c5d2eb6bf6cfd20b43ef9a5f4e
Seungjae Lee <saystone@gmail.com>
2017-05-30 18:48:26 +0900

    Define type to props arg

--- ts/components/Square.tsx
+++ ts/components/Square.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-function Square(props) {
+function Square(props: { value: string, onClick(): void }) {
   return (
     <button className="square" onClick={props.onClick}>
       {props.value}
